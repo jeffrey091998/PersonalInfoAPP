b30cf82e684b4cb0550adf70e28ce36e7c2336cc
jeffrey091998 <33153540+jeffrey091998@users.noreply.github.com>
2017-10-28 00:26:41 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function Save(){
+
+window.localStorage['fname'] = document.getElementById('fname') .value;
+document.getElementById('fname').value=null
+window.localStorage['lname'] = document.getElementById('lname') .value;
+document.getElementById('lname').value=null
+	
+window.localStorage['email'] = document.getElementById('email') .value;
+document.getElementById('email').value=null
+window.localStorage['num'] = document.getElementById('num') .value;
+document.getElementById('num').value=null
+
+window.localStorage['add'] = document.getElementById('add') .value;
+document.getElementById('add').value=null
+
+window.localStorage['city'] = document.getElementById('city') .value;
+document.getElementById('city').value=null
+
+window.localStorage['city'] = document.getElementById('city') .value;
+document.getElementById('city').value=null
+
+window.localStorage['drop'] = document.getElementById('drop') .value;
